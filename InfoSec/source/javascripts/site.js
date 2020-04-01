c99378a9561643d43c7b4c9931a18b4f2da6cda5
sandoval.65@osu.edu <4621n8745>
2020-04-01 10:23:07 -0400

    Added side panel for chat

--- InfoSec/source/javascripts/site.js
+++ InfoSec/source/javascripts/site.js
@@ -1,1 +1,11 @@
 // This is where it all goes :)
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
+  
